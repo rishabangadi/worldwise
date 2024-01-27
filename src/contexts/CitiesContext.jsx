81279edb8d5ee720a5063b5677edf84aee581613
Rishab Angadi <rishab@Rishabs-MacBook-Air.local>
2024-01-27 11:26:49 +0530

    Added api

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -6,7 +6,7 @@ import {
   useCallback,
 } from "react";
 
-const BASE_URL = "http://localhost:9000";
+const BASE_URL = "https://globetrack-api.onrender.com/api";
 
 const CitiesContext = createContext();
 
